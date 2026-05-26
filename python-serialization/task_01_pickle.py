e1a90eedfaf0344a49ed5d0d1d338eb42bcc3aa7
Rwanbt <barat.erwan@gmail.com>
2026-05-26 20:05:59 +0200

    Fix: logic pickle

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -4,31 +4,37 @@ This module provides functions to serialize a Python dictionary
 to a JSON file and deserialize a JSON file back to a Python dictionary.
 """
 
-import json
+import pickle
 
 
-def serialize_and_save_to_file(data, filename):
-    """
-    Serializes a Python dictionary and saves it to a JSON file.
+class CustomObject:
+    """CustomObject class"""
 
-    Args:
-        data (dict): The Python dictionary to serialize.
-        filename (str): The filename of the output JSON file.
-            If the file exists, it will be replaced.
-    """
-    with open(filename, "w", encoding="utf-8") as f:
-        json.dump(data, f)
+    def __init__(self, name, age, is_student):
+        """Init method"""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
 
+    def display(self):
+        """Display method"""
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
 
-def load_and_deserialize(filename):
-    """
-    Loads and deserializes data from a JSON file back into a Python dictionary.
+    def serialize(self, filename):
+        """Serialize method"""
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except Exception:
+            return None
 
-    Args:
-        filename (str): The filename of the input JSON file.
-
-    Returns:
-        dict: The deserialized Python dictionary from the file.
-    """
-    with open(filename, "r", encoding="utf-8") as f:
-        return json.load(f)
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize method"""
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except Exception:
+            return None
