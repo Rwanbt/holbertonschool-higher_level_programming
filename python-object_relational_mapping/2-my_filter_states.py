e91a860c74c112f81e29c99d02bd88d5c74ffb5b
Rwanbt <barat.erwan@gmail.com>
2026-06-09 00:53:49 +0200

    Fix: format my filter

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -19,9 +19,11 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(
+        sys.argv[4]
+    )
 
-    cursor.execute(query, (sys.argv[4],))
+    cursor.execute(query)
 
     rows = cursor.fetchall()
     for row in rows:
