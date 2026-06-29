78fa5cf6e5ec234ba323d264d87ff1c912742e13
Rwanbt <barat.erwan@gmail.com>
2026-06-29 15:55:20 +0200

    Fix: spaces print x times

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 const x = parseInt(process.argv[2], 10);
 
 if (isNaN(x)) {
-    console.log('Missing number of occurences');
+  console.log('Missing number of occurences');
 } else {
     for (let i = 0; i < x; i++) {
-        console.log('C is fun');
+    console.log('C is fun');
     }
 }
