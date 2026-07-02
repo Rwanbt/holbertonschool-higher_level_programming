ae1e61f4a5b08e7e9772221486ed7c8b6d2d49f6
Rwanbt <barat.erwan@gmail.com>
2026-07-02 18:47:15 +0200

    Feat: dynamic template, loops & conditions

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,23 @@
+import json
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/items")
+def items():
+    items_list = []
+
+    try:
+        with open("items.json", "r") as file:
+            data = json.load(file)
+            items_list = data.get("items", [])
+    except FileNotFoundError:
+        print("Le fichier items.json est introuvable.")
+
+    return render_template("items.html", items=items_list)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
